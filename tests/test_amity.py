e322b4712f3983419669b2987f9b8f90f99c3013
Andela-Sumba <daniel.sumba@andela.com>
2017-04-19 08:34:49 +0300

    Added the tests to test the create room and add person function in amity

---
+++ tests/test_amity.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+import os
+import unittest
+from amity.amity import Amity
+from amity.person import Person
+from amity.room import Room
+
+class TestAmity(unittest.TestCase):
+    """
+    Tests the amity system core functions
+    """
+    def test_create_room(self):
+        """Tests that amity creates rooms of either office or living space"""
+        pass
+
+    def test_amity_does_not_create_duplicte_rooms(self):
+        """Test that amity does not create duplicate rooms"""
+        pass
+
+    def test_add_person(self):
+        """Test that amity can add a person to a the amity system"""
+        pass
